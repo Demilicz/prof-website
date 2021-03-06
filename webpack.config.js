2a3eff7f0cadfd6748555454b8ebcca8c53e6b45
Demilicz <Necrolick92@gmail.com>
2021-03-06 21:48:26 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { LoaderOptionsPlugin } = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: "./js/app.js",
+  },
+  context: path.resolve(__dirname, 'src'),
+  devServer: {
+    compress: true,
+    open: true,
+    port: 9000,
+    contentBase: path.join(process.cwd(), 'dist'),
+    host: 'localhost',
+    historyApiFallback: true,
+    noInfo: false,
+    stats: 'minimal',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+
+            options: {
+              name: '[path][name].[ext]',
+              outputPath: 'images/'
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/,
+        loader: "file-loader",
+        options: {
+          limit: 10000,
+          name: "[name].[ext]",
+          outputPath: 'font/'
+        },
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+
+              // hmr: process.env.NODE_ENV === 'development'
+            }
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: './style.css'
+    })
+  ],
+
+  output: {
+    filename: '[name].[fullhash] .js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+};
